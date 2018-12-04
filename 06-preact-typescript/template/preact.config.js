b087cc5327e46f925798b9d761100a2083520db6
littleninja <sarah@grefalda.com>
2018-12-03 22:52:36 -0800

    Add script to render widget with hot module loader

--- 06-preact-typescript/template/preact.config.js
+++ 06-preact-typescript/template/preact.config.js
@@ -1,9 +1,15 @@
 export default (config, env, helpers) => {
+
   // make sure .ts and .tsx file extensions are included
   const extensions = config.resolve.extensions;
   if (!extensions.includes('.ts')) extensions.push('.ts');
   if (!extensions.includes('.tsx')) extensions.push('.tsx');
 
-  // dirty hack to replace babel-loader with ts-loader
-  config.module.rules[0] = { test: /\.tsx?$/, loader: "ts-loader" };
+  // remove babel loader(s)
+  let babelLoaders = helpers.getLoadersByName(config, 'babel-loader');
+  for (let loader of babelLoaders) {
+    config.module.rules.slice(loader.index, loader.index + 1);
+  }
+  // replace with typescript loader
+  config.module.rules.unshift({ test: /\.tsx?$/, loader: "ts-loader" });
 };
--- 06-preact-typescript/template/src/index.js
+++ 06-preact-typescript/template/src/index.js
@@ -1,1 +1,11 @@
-export { default as Widget } from './widget';
+import { h } from "preact";
+import habitat from "preact-habitat";
+
+import Widget from "./widget";
+
+let _habitat = habitat(Widget);
+
+_habitat.render({
+  selector: '[data-widget-host="habitat"]',
+  clean: true
+});
